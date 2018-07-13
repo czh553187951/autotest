5efcb544605c3b881ab3555724cbd0e3089013f3
chenzhenhua <553187951@qq.com>
2018-07-13 17:48:44 +0800

    day2.1

--- Chapter/src/main/java/com/course/testng/multiThread/MultiThreadTest.java
+++ Chapter/src/main/java/com/course/testng/multiThread/MultiThreadTest.java
@@ -1,4 +1,13 @@
 package com.course.testng.multiThread;
 
+import org.testng.annotations.Test;
+
 public class MultiThreadTest {
+    @Test(invocationCount = 10,threadPoolSize = 3)
+    public void test(){
+        System.out.println("a");
+        System.out.printf("线程ID是：%s%n",Thread.currentThread().getId());
+
+    }
+
 }
--- Chapter/src/main/java/com/course/testng/multiThread/MultiThreadXMLTest.java
+++ Chapter/src/main/java/com/course/testng/multiThread/MultiThreadXMLTest.java
@@ -1,4 +1,20 @@
 package com.course.testng.multiThread;
 
+import org.testng.annotations.Test;
+
 public class MultiThreadXMLTest {
+    @Test
+    public void test1(){
+        System.out.printf("线程ID是：%s%n",Thread.currentThread().getId());
+    }
+
+    @Test
+    public void test2(){
+        System.out.printf("线程ID是：%s%n",Thread.currentThread().getId());
+    }
+
+    @Test
+    public void test3(){
+        System.out.printf("线程ID是：%s%n",Thread.currentThread().getId());
+    }
 }
