5071b2cbc579ca383c201501c081eff76e0ba32a
chenzhenhua <553187951@qq.com>
2018-09-19 10:39:51 +0800

    cilent

--- Chapter3/src/main/java/com/httpclient/cookies/MyCookiesForGet.java
+++ Chapter3/src/main/java/com/httpclient/cookies/MyCookiesForGet.java
@@ -1,14 +1,17 @@
 package com.httpclient.cookies;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.client.CookieStore;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.cookie.Cookie;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -16,6 +19,8 @@ public class MyCookiesForGet {
 
     private String url;
     private ResourceBundle bundle;
+    //用来存取COOKIES
+    private  CookieStore store;
 
     @BeforeTest
     public void beforeTest(){
@@ -29,10 +34,46 @@ public class MyCookiesForGet {
         String uri=bundle.getString("getcookies.uri");
         String testurl=this.url+ uri;
         HttpGet get=new HttpGet(testurl);
-        HttpClient client=new DefaultHttpClient();
+        DefaultHttpClient client=new DefaultHttpClient();
         HttpResponse response = client.execute(get);
         result= EntityUtils.toString(response.getEntity(),"utf-8");
         System.out.println(result);
+
+
+        //获取cookies
+       this.store=client.getCookieStore();
+        List<Cookie> cookieList=store.getCookies();
+
+        for (Cookie cookie:cookieList){
+            String name=cookie.getName();
+            String value=cookie.getValue();
+            System.out.println("cookie name:"+name+"  cookie value: "+value);
+        }
     }
+         @Test(dependsOnMethods = {"testgetCookies"})
+         public void testGetWithCookies() throws IOException {
+
+        String uri=bundle.getString("test.get.with.cookies");
+             //拼接地址
+             String testurl=this.url+ uri;
+        HttpGet get =new HttpGet(testurl);
+        DefaultHttpClient client=new DefaultHttpClient();
+
+        //设置cookies信息
+        client.setCookieStore(this.store);
+
+        HttpResponse response = client.execute(get);
+
+        //获取状态码
+        int statusCode =response.getStatusLine().getStatusCode();
+        System.out.println("status="+statusCode);
+
+        if (statusCode==200){
+            String result= EntityUtils.toString(response.getEntity(),"utf-8");
+            System.out.println(result);
+        }
+
+    }
+
 
 }
